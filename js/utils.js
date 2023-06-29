d2faefb93c0f73e2e9e4dd14886012782de32905
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-06-29 00:24:23 -0400

    styled the movie card

--- js/utils.js
+++ js/utils.js
@@ -15,13 +15,19 @@ function createMovieCard(movie, imgSrc, descriptionText) {
   return `    
       <div class="movie-card">
         <img src=${poster} alt="${title}" class="movie-img">
-        <h3 class="movie-title">${title}</h3>
-        <img src="/images/filledStarIcon.png" alt="yellow color filled Star Icon" class="movie-rating-icon" />
-        <p class="movie-rating">${rating}</p>
-        <p class="movie-duration">${duration}</p>
-        <p class="movie-genre">${genre}</p>
-        <img src="${imgSrc}" class="action-icon" id=${movieId} /> 
-        <p class="description-text">${descriptionText}</p>
+        <div class="first-row">
+          <h3 class="movie-title">${title}</h3>
+          <img src="/images/filledStarIcon.png" alt="yellow color filled Star Icon" class="movie-rating-icon" />
+          <p class="movie-rating">${rating}</p>
+        </div>
+        <div class="second-row">
+          <p class="movie-duration">${duration}</p>
+          <p class="movie-genre">${genre}</p>
+          <div class="watchlist-section">
+            <img src="${imgSrc}" class="action-icon" id=${movieId} /> 
+            <p class="description-text">${descriptionText}</p>
+          </div>
+        </div>
         <p class="movie-plot">${plot}</p>
       </div>   
     `;
